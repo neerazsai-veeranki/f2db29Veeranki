9e3e6e5933078e15865397b17ebaf6dc90b2908b
neerazveeranki <44938842+neerazveeranki@users.noreply.github.com>
2022-11-30 17:43:26 -0600

    added validation

--- models/tunnels.js
+++ models/tunnels.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose") 
 
 const tunnelSchema = mongoose.Schema({ 
-    len_of_tunnel: Number, 
-    no_of_lanes: Number, 
-    tunnel_name: String,
-    is_operational:Boolean 
+    len_of_tunnel: { type: Number, required: true, min: 1 }, 
+    no_of_lanes: { type: Number, required: true, min: 1 }, 
+    tunnel_name: { type: String, required: true },
+    is_operational: { type: Boolean, required: true }
 }) 
  
 module.exports = mongoose.model("Tunnel", 
