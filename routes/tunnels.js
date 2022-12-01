62998a661ba4c25d62533d8d7a410993651dcdb9
neerazveeranki <44938842+neerazveeranki@users.noreply.github.com>
2022-11-30 18:29:39 -0600

    added bonus

--- routes/tunnels.js
+++ routes/tunnels.js
@@ -17,7 +17,7 @@ const secured = (req, res, next) => {
 router.get('/', tunnels_controllers.tunnel_view_all_Page);
 
 /* CREATE a new Tunnel obj. */
-router.get('/create', tunnels_controllers.tunnel_create_Page);
+router.get('/create', secured, tunnels_controllers.tunnel_create_Page);
 
 // PUT/UPDATE request to update Tunnel.  
 router.get('/update', secured, tunnels_controllers.tunnel_update_Page); 
@@ -26,7 +26,7 @@ router.get('/update', secured, tunnels_controllers.tunnel_update_Page);
 router.get('/detail', tunnels_controllers.tunnel_view_one_Page); 
 
 // DELETE request for one Tunnel. 
-router.get('/delete', tunnels_controllers.tunnel_delete_Page); 
+router.get('/delete', secured, tunnels_controllers.tunnel_delete_Page); 
 
 // router.get('/', function(req, res, next) {
 //   res.render('tunnels', { title: 'Search Results' });
